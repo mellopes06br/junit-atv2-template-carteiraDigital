3104c2b24aea544b1fd503c7cca74d7d2935347c
mellopes06br <163359225+mellopes06br@users.noreply.github.com>
2025-09-15 19:53:19 +0000

    Início testes

--- src/test/java/Deposito.java
+++ src/test/java/Deposito.java
@@ -1,18 +1,32 @@
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import com.example.DigitalWallet;
 
 
 class Deposito {
         
-        void deveDepositarValoresValidos(double amount) {
-            
-        }
+    DigitalWallet digitalWallet;
 
+    @BeforeEach                                                               //cria um objeto que pode ser utilizado em todos os testes
+    public void setUp(){
+        digitalWallet = new DigitalWallet("Mel", 10);
+    }
+
+        @ParameterizedTest
+        @ValueSource(doubles = {10.0, 0.01, 999.99})
+        void deveDepositarValoresValidos(double amount) {
+            double initialValue = digitalWallet.getBalance();                   //traz o valor inicial 
+            digitalWallet.deposit(amount);                                      //valor do depósito
+            assertEquals(amount + initialValue, digitalWallet.getBalance());    //realiza e traz o valor pós depósito
+        }
         
         void deveLancarExcecaoParaDepositoInvalido(double amount) {
             
--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -1,16 +1,26 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.api.Test;
 
 import com.example.DigitalWallet;
 
 class SaldoInicial {
+
+       @Test
         void deveConfigurarSaldoInicialCorreto() {
-            
+           DigitalWallet digitalWallet = new DigitalWallet("Mel", 0); 
+           assertEquals(0, digitalWallet.getBalance());
         }
 
-        void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
+        @ParameterizedTest
+        @ValueSource(doubles={-24,-2,-48})
+        void deveLancarExcecaoParaSaldoInicialNegativo(double balance) {
+            assertThrows(IllegalArgumentException.class, ()->{
+               DigitalWallet digitalWallet = new DigitalWallet("Mel", balance); 
+            });
         }
     }
