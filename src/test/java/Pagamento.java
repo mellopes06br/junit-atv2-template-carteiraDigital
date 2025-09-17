dbd63b38329c90817d1cf2c428f6915c04113442
mellopes06br <163359225+mellopes06br@users.noreply.github.com>
2025-09-17 19:45:04 +0000

    test pagamento

--- src/test/java/Pagamento.java
+++ src/test/java/Pagamento.java
@@ -13,16 +13,39 @@ import com.example.DigitalWallet;
 
 public class Pagamento {
     
+    @ParameterizedTest
+    @CsvSource({"100.0, 30.00, true", "50.0, 80.0, false", "10.0, 10.0, true"})
     void pagamentoComCarteiraVerificadaENaoBloqueada(double inicial, double valor, boolean esperado) {
+
+        DigitalWallet digitalWallet = new DigitalWallet("Mel", inicial);                         //cria carteir acom saldo inicial
         
+        digitalWallet.verify();       //verifica a carteira
+        digitalWallet.unlock();       //desbloqueia a carteira
+
+        assumeTrue(digitalWallet.isVerified() && !digitalWallet.isLocked()); //carteira verificada e não bloqueada.
+        assertEquals(esperado, digitalWallet.pay(valor));
     }
 
-    
+    @ParameterizedTest
+    @ValueSource(doubles = {-10, 0, -0.1})
     void deveLancarExcecaoParaPagamentoInvalido(double valor) {
-        
-    }
+        DigitalWallet digitalWallet = new DigitalWallet("Mel", 100);
 
-    void deveLancarSeNaoVerificadaOuBloqueada() {
+        digitalWallet.unlock();        //verifica a carteira
+        digitalWallet.verify();       //desbloqueia a carteira
         
+        assumeTrue(digitalWallet.isVerified() && !digitalWallet.isLocked()); //carteira verificada e não bloqueada.
+        assertThrows(IllegalArgumentException.class, ()->{
+            digitalWallet.pay(valor);
+        });
+
+    }
+   
+    @Test
+    void deveLancarSeNaoVerificadaOuBloqueada() {
+        DigitalWallet digitalWallet = new DigitalWallet("Mel", 100);
+        assertThrows(IllegalStateException.class, ()->{
+            digitalWallet.pay(10);
+        });
     }
 }
